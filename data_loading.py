78e5ac6359f73164457120fb59672b760512fa97
Viktor <todosijevicviktor998@gmail.com>
2021-08-13 00:07:42 +0200

    Added constants.py

--- data_loading.py
+++ data_loading.py
@@ -1,6 +1,10 @@
-from paths import TRAIN_DATA_PATH
+import os
+import numpy as np
+
+from paths import TRAIN_DATA_PATH, DATA_FILENAME
 
 DATA_PATH = TRAIN_DATA_PATH
+DATASET = 'birds'
 
 if __name__ == '__main__':
-    pass
+    data = np.load(os.path.join(DATA_PATH, DATASET, DATA_FILENAME + '.npy'))
