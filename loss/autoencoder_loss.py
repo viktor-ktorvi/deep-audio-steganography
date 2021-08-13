81c0f80e2ff4382f3c0a7fe797299d0d82a62d3f
Viktor <todosijevicviktor998@gmail.com>
2021-08-13 12:54:40 +0200

    Added autoencoder.py and autoencoder_loss.py

---
+++ loss/autoencoder_loss.py
@@ -0,0 +1,16 @@
+from torch import nn
+
+
+class AutoEncoderLoss(nn.Module):
+    def __init__(self):
+        super(AutoEncoderLoss, self).__init__()
+
+        self.encoder_criterion = nn.MSELoss()
+        self.decoder_criterion = nn.BCELoss()
+
+    def forward(self, modified_audio, original_audio, reconstructed_message, original_message):
+
+        encoder_loss = self.encoder_criterion(modified_audio, original_audio)
+        decoder_loss = self.decoder_criterion(reconstructed_message, original_message)
+
+        return encoder_loss + decoder_loss
---
+++ network_modules/autoencoder.py
@@ -0,0 +1,38 @@
+import numpy as np
+import torch
+from torch import nn
+
+from network_modules.encoder import Encoder
+from network_modules.decoder import Decoder
+
+from data_loading import prepare_messages
+from constants.constants import CHANNELS, KERNELS, SIGNAL_LEN, DEVICE
+from constants.parameters import MESSAGE_LEN, BOTTLENECK_CHANNEL_SIZE, STRIDES
+
+
+class AutoEncoder(nn.Module):
+    def __init__(self, strides):
+        super(AutoEncoder, self).__init__()
+
+        self.encoder = Encoder(strides=strides)
+        self.decoder = Decoder()
+
+    def forward(self, x, message_reshaped):
+        modified_audio = self.encoder(x, message_reshaped)
+        reconstructed_message = self.decoder(modified_audio)
+
+        return reconstructed_message, modified_audio
+
+
+if __name__ == '__main__':
+    autoencoder = AutoEncoder(strides=STRIDES).to(DEVICE)
+    print(autoencoder)
+
+    batch_size = 17
+
+    messages = np.random.randint(low=0, high=2, size=(batch_size, MESSAGE_LEN))
+    messages_reshaped = prepare_messages(messages)
+    messages_reshaped_tensor = torch.tensor(messages_reshaped).to(DEVICE)
+
+    x = torch.randn(size=(batch_size, 1, SIGNAL_LEN)).to(DEVICE)
+    reconstructed_message, modified_audio = autoencoder(x, messages_reshaped_tensor)
