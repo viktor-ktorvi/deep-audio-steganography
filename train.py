19dc11d63aa245613088eb61956990cfd4136de6
Viktor <todosijevicviktor998@gmail.com>
2021-08-13 11:41:06 +0200

    Added manual seeds

--- train.py
+++ train.py
@@ -1,11 +1,16 @@
 import torch
 from torch.utils.data import DataLoader
+import numpy as np
 
 from data_loading import get_dataset
 from constants import DEVICE
 from parameters import BATCH_SIZE
 
 if __name__ == '__main__':
+    np.random.seed(1)
+    torch.manual_seed(1)
+    torch.cuda.manual_seed(1)
+
     train_set, validation_set, test_set = get_dataset()
 
     train_dataloader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True)
