8b8461782b3912033e77360e447f988d34c0a994
Viktor <todosijevicviktor998@gmail.com>
2021-08-16 22:19:12 +0200

    Checked accuracy in the inference.py script, everything is great!

--- inference.py
+++ inference.py
@@ -3,8 +3,11 @@ import json
 import numpy as np
 import torch
 
+from torch.utils.data import DataLoader
+
 from network_modules.autoencoder import AutoEncoder
 from data_loading import get_inference_data
+from utils.accuracy import pass_data_through, calc_mean_accuracy
 
 from constants.paths import SAVE_MODELS_PATH, MODEL_PARAMETERS_FOLDER, INFERENCE_DATA_FOLDER
 from constants.parameters import TRAINING_PARAMETERS_JSON
@@ -16,6 +19,7 @@ MODEL_EXTENSION = '.pt'
 DATASET = 'birds'
 
 if __name__ == '__main__':
+    # %% Loading parameters and the model
     MODEL_FOLDER_PATH = os.path.join(SAVE_MODELS_PATH, MODEL_TO_LOAD)
     MODEL_PATH = os.path.join(MODEL_FOLDER_PATH, MODEL_NAME + MODEL_EXTENSION)
     PARAMETERS_PATH = os.path.join(MODEL_FOLDER_PATH, MODEL_PARAMETERS_FOLDER)
@@ -32,7 +36,20 @@ if __name__ == '__main__':
     model = AutoEncoder(strides=strides, bottleneck_channel_size=bottleneck_channel_size).to(DEVICE)
     model.load_state_dict(torch.load(MODEL_PATH))
 
-    test_dataset = get_inference_data(data_path=INFERENCE_DATA_PATH,
-                                      num_signals=None,
-                                      high=training_parameters['HIGH'],
-                                      bottleneck_channel_size=training_parameters['BOTTLENECK_CHANNEL_SIZE'])
+    # %% Loading data
+    data = get_inference_data(data_path=INFERENCE_DATA_PATH,
+                              num_signals=None,
+                              high=training_parameters['HIGH'],
+                              bottleneck_channel_size=training_parameters['BOTTLENECK_CHANNEL_SIZE'])
+
+    dataloader = DataLoader(data, batch_size=len(data), shuffle=True)
+    original_messages, reconstructed_messages, original_audio, modified_audio = pass_data_through(model, dataloader)
+    modified_audio = modified_audio.squeeze()
+
+    # %% Accuracy
+    test_acc = calc_mean_accuracy(reconstructed_messages, original_messages, high=training_parameters['HIGH'])
+
+    print("Test accuracy is {:3.2f} %".format(test_acc * 100))
+    # TODO  save the stego next to the original and name it after the model,
+    #  see if everything is ok
+    #  do some tests as in SNR, spectrograms, noise and quantization seristance
